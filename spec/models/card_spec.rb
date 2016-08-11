d0ae53140aaa4394ca64117c581fa1ce7784b47c
dabonka <dabonka@yandex.ru>
2016-08-11 23:44:31 +0400

    Add test to model

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -25,15 +25,60 @@ describe "Card" do
       card = Card.new(translated_text: " hause ")
       expect(card.check_translation("hause")).to be true
     end
+  end
 
     describe "success" do
-      true
+      it "when first check is success" do
+        @card = FactoryGirl.create(:card)
+        @card.review_date = Time.current.strftime("%d/%m/%Y")
+        @card.success
+        expect(@card.level).to eq 1
+        expect((@card.review_date).strftime("%d/%m/%Y")).to eq (Time.current + 12.hour).strftime("%d/%m/%Y")
+      end
+
+      it "when second check is success" do
+        @card = FactoryGirl.create(:card)
+        2.times {@card.success}
+        expect(@card.level).to eq 2
+        expect((@card.review_date).strftime("%d/%m/%Y")).to eq (Time.current + 3.days).strftime("%d/%m/%Y")
+      end
+
+      it "when third check is success" do
+        @card = FactoryGirl.create(:card)
+        3.times {@card.success}
+        expect(@card.level).to eq 3
+        expect((@card.review_date).strftime("%d/%m/%Y")).to eq (Time.current + 1.week).strftime("%d/%m/%Y")
+      end
+
     end
+
 
     describe "failed" do
-      true
+      it "when first check is failed" do
+        @card = FactoryGirl.create(:card)
+        @card.level = 3
+        @card.failed
+        expect(@card.level).to eq 3
+        expect(@card.fail_counter).to eq 1
+      end
+
+      it "when second check is faild" do
+        @card = FactoryGirl.create(:card)
+        @card.level = 3
+        2.times {@card.failed}
+        expect(@card.level).to eq 3
+        expect(@card.fail_counter).to eq 2
+      end
+
+      it "when third check is faild" do
+        @card = FactoryGirl.create(:card)
+        @card.level = 3
+        3.times {@card.failed}
+        expect(@card.level).to eq 1
+        expect(@card.fail_counter).to eq 0
+      end
     end
 
 
-  end
+
 end
