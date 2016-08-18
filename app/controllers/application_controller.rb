e8b43a96b8d432916dcb8af2a1ad94ebce49e2e1
dabonka <dabonka@yandex.ru>
2016-08-18 23:11:26 +0400

    some fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+  include HttpAcceptLanguage::AutoLocale
+  require 'http_accept_language'
+
+  before_filter :set_locale
+
   private
+
+  def set_locale
+    I18n.locale = if current_user
+               current_user.locale
+             elsif params[:locale]
+               session[:locale] = params[:locale]
+             elsif session[:locale]
+               session[:locale]
+             else
+               http_accept_language.compatible_language_from(I18n.available_locales)
+             end
+  end
 
   def require_login
     unless logged_in?
@@ -10,5 +27,7 @@ class ApplicationController < ActionController::Base
       redirect_to login_path
     end
   end
+
+
 end
 
